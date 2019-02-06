3aaf8bd640e7602a04320f5f49a995fcd1a724f1
iammanish007 <qa@gmail.com>
2019-02-06 23:56:49 +0530

    pattern + array + string

--- src/Patterns/Pattern14.java
+++ src/Patterns/Pattern14.java
@@ -9,8 +9,13 @@ package Patterns;
 //*
 
 public class Pattern14 {
-
 	public static void main(String[] args) {
-		
+		int size = 3;
+		for (int i = size; i >= -size; i--) {
+			for (int j = size; j >= Math.abs(i); j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 }
---
+++ src/String/String01_ReverseAString.java
@@ -0,0 +1,17 @@
+package String;
+
+//Input: "I am Manish"
+//Output: "hsinaM ma I"
+
+public class String01_ReverseAString {
+
+	public static void main(String[] args) {
+		String str = "I am Manish", reverse="";
+		System.out.println("Given string: "+str);
+		for(int i=0;i<str.length();i++){
+			reverse= str.charAt(i)+reverse;
+		}
+		System.out.println("Reversed string: "+reverse);
+	}
+
+}
