625d0bf9d05478ff61738f572657bc8110ff171e
iammanish007 <qa@gmail.com>
2019-02-19 06:16:08 +0530

    pattern 16

--- src/Array/Array01SumOfValuesOfAnArray.java
+++ src/Array/Array01SumOfValuesOfAnArray.java
@@ -2,13 +2,13 @@ package Array;
 
 public class Array01SumOfValuesOfAnArray {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		int sum = 0;
-		int arr[] = {4,9,5,2,6};
-		for(int i =0; i<arr.length;i++){
+		int arr[] = { 4, 9, 5, 2, 6 };
+		for (int i = 0; i < arr.length; i++) {
 			sum = sum + arr[i];
 		}
-		System.out.println("Sum of array: "+sum);
+		System.out.println("Sum of array: " + sum);
 
 	}
 
--- src/String/String01_ReverseAString.java
+++ src/String/String01_ReverseAString.java
@@ -4,14 +4,12 @@ package String;
 //Output: "hsinaM ma I"
 
 public class String01_ReverseAString {
-
 	public static void main(String[] args) {
-		String str = "I am Manish", reverse="";
-		System.out.println("Given string: "+str);
-		for(int i=0;i<str.length();i++){
-			reverse= str.charAt(i)+reverse;
+		String str = "I am Manish", reverse = "";
+		System.out.println("Given string: " + str);
+		for (int i = 0; i < str.length(); i++) {
+			reverse = str.charAt(i) + reverse;
 		}
-		System.out.println("Reversed string: "+reverse);
+		System.out.println("Reversed string: " + reverse);
 	}
-
 }
